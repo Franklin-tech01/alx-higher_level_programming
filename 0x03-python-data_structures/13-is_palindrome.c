60afb6a1bd83568b7c0e3d84746e0c4992e2c886
Franklin-tech01 <franklinmike2021@gmail.com>
2023-01-01 20:04:19 +0000

    C again?

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,33 @@
-include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
+ * is_palindrome - test if is a palindrome
+ * @head: firts node of list
+ * Return: 1 if is palindrome, 0 for no
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *buscar;
+	int con1 = 0, con2 = 0;
+	int arr[4096];
 
-	if (*head == NULL || (*head)->next == NULL)
+
+	if (!head)
+		return (0);
+	buscar = *head;
+	if (!*head || (*head)->next == NULL)
 		return (1);
 
-	while (1)
+	for (con1 = 0; buscar; buscar = buscar->next, con1++)
+		arr[con1] = buscar->n;
+	for (con1--; con1 > con2; con1--, con2++)
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
-
-	reverse_listint(&dup);
-
-	while (dup && temp)
-	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
+		if (arr[con2] == arr[con1])
+			;
 		else
 			return (0);
 	}
-
-	if (!dup)
-		return (1);
-
-	return (0);
+	return (1);
 }
