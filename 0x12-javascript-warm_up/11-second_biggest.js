4748fed90b429a5208d92063d7b15722f667b787
Franklin-tech01 <franklinmike2021@gmail.com>
2023-03-18 15:39:35 +0000

    js seven

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-if (!process.argv[2] || !process.argv[3]) {
-    console.log('0');
-  } else {
-    let i = 2;
-    const myArray = [];
-    while (process.argv[i]) {
-      myArray[i - 2] = process.argv[i];
-      i++;
-    }
-    const secondLargest = myArray.sort(function (a, b) { return a - b; })[myArray.length - 2];
-    console.log(secondLargest);
-  }
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
