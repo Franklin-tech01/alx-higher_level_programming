415dfbbf7ddad1487eff86a4d2438ac74d422c97
Franklin-tech01 <franklinmike2021@gmail.com>
2023-03-18 15:35:50 +0000

    js five

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
-  }
-  const a = parseInt(process.argv[2], 10);
-  const b = parseInt(process.argv[3], 10);
-  console.log(add(a, b));
+  const c = a + b;
+  console.log(c);
+}
+
+add(Number(process.argv[2]), Number(process.argv[3]));
