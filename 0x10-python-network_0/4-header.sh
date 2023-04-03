18bdc71f4b5c55fd1baa2bf7df88b7c9b75efa62
Franklin-tech01 <franklinmike2021@gmail.com>
2023-04-03 16:37:57 -0700

    fixed nuber 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s -H "X-School-User-Id:98" "$1"
