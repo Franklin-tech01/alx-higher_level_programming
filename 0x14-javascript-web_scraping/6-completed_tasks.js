03b32507d850fc2339e3d0b43ce20b957b3dc20c
Franklin-tech01 <franklinmike2021@gmail.com>
2023-06-10 00:03:44 +0000

    fixed 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,29 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+
+  const completedTasksByUser = {};
+
+  for (const todo of todos) {
+    if (todo.completed) {
+      const userId = todo.userId;
+      if (userId in completedTasksByUser) {
+        completedTasksByUser[userId]++;
+      } else {
+        completedTasksByUser[userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+
+  console.log(completedTasksByUser);
 });
+
